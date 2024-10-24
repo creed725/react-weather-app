295729ff38d4dd726ef80a7dbc2340f03ecb4b26
Ciara R. <100184461+creed725@users.noreply.github.com>
2024-10-23 20:08:52 -0500

    Wrapped Components in a div and created a footer for Open-source

--- src/App.js
+++ src/App.js
@@ -6,26 +6,28 @@ import "./containerStyle.css";
 
 function App() {
   return (
-    <div className="App container">
-      <SearchForm />
-      <div className="row">
-        <div className="col-md-6">
-          <Weather />
+    <div className="App">
+      <div className="container">
+        <SearchForm />
+        <div className="row">
+          <div className="col-md-6">
+            <Weather />
+          </div>
+          <div className="col-md-6">
+            <Forecast />
+          </div>
+          {/*GitHub Repository Link Section*/}
+          <footer id="author">
+            <a
+              href="https://github.com/creed725/react-weather-app.git"
+              target="_blank"
+              rel="noopener noreferrer" //This is a security best practice
+            >
+              Open-source
+            </a>
+            coded by Ciara Reed 👩🏽‍💻
+          </footer>
         </div>
-        <div className="col-md-6">
-          <Forecast />
-        </div>
-        {/*GitHub Repository Link Section*/}
-        <h3 id="author">
-          <a
-            href="https://github.com/creed725/react-weather-app.git"
-            target="_blank"
-            rel="noopener noreferrer" //This is a security best practice
-          >
-            Open-source
-          </a>
-          coded by Ciara Reed 👩🏽‍💻
-        </h3>
       </div>
     </div>
   );
