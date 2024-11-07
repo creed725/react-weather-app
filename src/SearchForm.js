2e1535af514e4afb6b6a20fc0a69f9ab465c46c1
Ciara R. <100184461+creed725@users.noreply.github.com>
2024-11-06 22:57:42 -0600

    Added row and columns around search form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,28 +2,36 @@ import React from "react";
 
 export default function SearchForm() {
   return (
-    <div className="buttons mb-3">
-      <form id="search-form">
-        <input
-          type="text"
-          placeholder="Enter a City.."
-          autoComplete="off"
-          autoFocus="on"
-          id="search-input"
-        />
-        <p className="current-location">
-          <button type="submit" className="btn btn-primary" id="search-button">
-            Search
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary ms-2"
-            id="current-locationbtn"
-          >
-            Current Location
-          </button>
-        </p>
-      </form>
+    <div className="">
+      <div className=" row buttons mb-3">
+        <div className="col-7">
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Enter a City.."
+              autoComplete="off"
+              autoFocus="on"
+              id="search-input"
+            />
+            <p className="current-location">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                id="search-button"
+              >
+                Search
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary ms-2"
+                id="current-locationbtn"
+              >
+                Current Location
+              </button>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
