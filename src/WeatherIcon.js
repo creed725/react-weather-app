9bd72b341caa535790bdef8961a4dd8fa6cc59d4
Ciara R. <100184461+creed725@users.noreply.github.com>
2024-12-11 19:05:17 -0600

    Updated WeatherIcon.js file

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function WeatherIcon({ icon }) {
+export default function WeatherIcon({ icon, size }) {
   return (
     <ReactAnimatedWeather
       icon={icon} //Use the icon passed as a prop
       color="black"
-      size={icon.size}
+      size={size}
       animate={true}
     />
   );
