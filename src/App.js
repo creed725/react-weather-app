dfd076815577af1c3202bfa58a440a32e775a41c
Ciara R. <100184461+creed725@users.noreply.github.com>
2025-01-06 18:47:40 -0600

    Update App.js file to change the layout
    -Removed bootstrap card group
    -Removed folder path and the containerStyle  css folder path

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import Weather from "./Weather";
-//import Forecast from "./Forecast";
-
-//import "./containerStyle.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Weather defaultCity="Seattle" />
+
         {/*GitHub Repository Link Section*/}
-        <div>
-          {" "}
-          <footer id="author">
-            <a
-              href="https://github.com/creed725/react-weather-app.git"
-              target="_blank"
-              rel="noopener noreferrer" //This is a security best practice
-            >
-              Open-source
-            </a>{" "}
-            coded by Ciara Reed 👩🏽‍💻
-          </footer>
-        </div>
+        <footer id="author">
+          <a
+            href="https://github.com/creed725/react-weather-app.git"
+            target="_blank"
+            rel="noopener noreferrer" //This is a security best practice
+          >
+            Open-source
+          </a>{" "}
+          coded by Ciara Reed 👩🏽‍💻
+        </footer>
       </div>
     </div>
   );
