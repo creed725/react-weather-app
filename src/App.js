99af1711bed260ca61cff47dafc6ae53636edc38
Ciara R. <100184461+creed725@users.noreply.github.com>
2025-01-06 18:31:52 -0600

    Update App.js file to change the layout
    -Removed the bootstrap card group

--- src/App.js
+++ src/App.js
@@ -7,36 +7,21 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="card-group">
-              <div className="card">
-                <h1 className="Weather">
-                  {" "}
-                  <span>Weather</span>
-                </h1>
-                <div className="row">
-                  <div className="col-md-6">
-                    <Weather defaultCity="Seattle" />
-                  </div>
-                  <div className="col-md-6">{/*<Forecast /> */}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <Weather defaultCity="Seattle" />
         {/*GitHub Repository Link Section*/}
-        <footer id="author">
-          <a
-            href="https://github.com/creed725/react-weather-app.git"
-            target="_blank"
-            rel="noopener noreferrer" //This is a security best practice
-          >
-            Open-source
-          </a>{" "}
-          coded by Ciara Reed 👩🏽‍💻
-        </footer>
+        <div>
+          {" "}
+          <footer id="author">
+            <a
+              href="https://github.com/creed725/react-weather-app.git"
+              target="_blank"
+              rel="noopener noreferrer" //This is a security best practice
+            >
+              Open-source
+            </a>{" "}
+            coded by Ciara Reed 👩🏽‍💻
+          </footer>
+        </div>
       </div>
     </div>
   );
